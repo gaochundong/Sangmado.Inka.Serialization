35342ad8aec3fcf6d0e39aa29fcf3720413eb4b5
gaochundong <gaochundong@hotmail.com>
2017-08-16 14:13:38 +0800

    adding unit test cases

--- Tests/Sangmado.Inka.Serialization.UnitTests/Converters/XmlConvertUnitTest.cs
+++ Tests/Sangmado.Inka.Serialization.UnitTests/Converters/XmlConvertUnitTest.cs
@@ -100,5 +100,98 @@ namespace Sangmado.Inka.Serialization.UnitTests
 
             Assert.AreEqual(string.CompareOrdinal(expected, actual), 0);
         }
+
+        [TestMethod]
+        public void TestDeserializeObject_EmptySetting_EmptyNamespaces_NotIndented()
+        {
+            string xml = EmbeddedResourceLoader.LoadAsString(
+                @"Sangmado.Inka.Serialization.UnitTests.TestXml.Employee-DennisGao-NotIndented.xml");
+
+            var expected = XmlConvert.DeserializeObject<Employee>(xml);
+
+            var actual = new Employee()
+            {
+                Company = new Company()
+                {
+                    Id = 123,
+                    Name = "Sangmado",
+                },
+                Person = new Person()
+                {
+                    Id = 11111,
+                    FullName = "Dennis Gao",
+                    FirstName = "Dennis",
+                    LastName = "Gao",
+                },
+            };
+
+            Assert.IsNotNull(expected);
+            Assert.IsNotNull(expected.Company);
+            Assert.IsNotNull(expected.Person);
+            Assert.AreEqual(expected.Company.Name, actual.Company.Name);
+            Assert.AreEqual(expected.Person.FullName, actual.Person.FullName);
+        }
+
+        [TestMethod]
+        public void TestDeserializeObject_Setting_EmptyNamespaces_Indented()
+        {
+            string xml = EmbeddedResourceLoader.LoadAsString(
+                @"Sangmado.Inka.Serialization.UnitTests.TestXml.Employee-DennisGao-Indented.xml");
+
+            var expected = XmlConvert.DeserializeObject<Employee>(xml);
+
+            var actual = new Employee()
+            {
+                Company = new Company()
+                {
+                    Id = 123,
+                    Name = "Sangmado",
+                },
+                Person = new Person()
+                {
+                    Id = 11111,
+                    FullName = "Dennis Gao",
+                    FirstName = "Dennis",
+                    LastName = "Gao",
+                },
+            };
+
+            Assert.IsNotNull(expected);
+            Assert.IsNotNull(expected.Company);
+            Assert.IsNotNull(expected.Person);
+            Assert.AreEqual(expected.Company.Name, actual.Company.Name);
+            Assert.AreEqual(expected.Person.FullName, actual.Person.FullName);
+        }
+
+        [TestMethod]
+        public void TestDeserializeObject_Setting_Namespaces_Indented_OmitXmlDeclaration()
+        {
+            string xml = EmbeddedResourceLoader.LoadAsString(
+                @"Sangmado.Inka.Serialization.UnitTests.TestXml.Employee-DennisGao-Indented-OmitXmlDeclaration.xml");
+
+            var expected = XmlConvert.DeserializeObject<Employee>(xml);
+
+            var actual = new Employee()
+            {
+                Company = new Company()
+                {
+                    Id = 123,
+                    Name = "Sangmado",
+                },
+                Person = new Person()
+                {
+                    Id = 11111,
+                    FullName = "Dennis Gao",
+                    FirstName = "Dennis",
+                    LastName = "Gao",
+                },
+            };
+
+            Assert.IsNotNull(expected);
+            Assert.IsNotNull(expected.Company);
+            Assert.IsNotNull(expected.Person);
+            Assert.AreEqual(expected.Company.Name, actual.Company.Name);
+            Assert.AreEqual(expected.Person.FullName, actual.Person.FullName);
+        }
     }
 }
